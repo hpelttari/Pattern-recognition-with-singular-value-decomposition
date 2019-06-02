091a48947700adbd4f58a394d2be464161a20de6
Hannu <hannu.pelttari@gmail.com>
2019-06-02 12:03:37 +0300

    Refactor and generalize code

--- final_project.py
+++ final_project.py
@@ -1,65 +1,67 @@
 import numpy as np
 import mnist
-from matplotlib import pyplot as plt
 from numpy.linalg import svd
-from functions import visualize_eigenpatterns, classify_image, create_masks, invert_colors
-from PIL import Image
+from functions import create_train_test_split, classify_images, assess_classification_results, visualize_eigenpatterns, classify_image, create_masks, test_performance, load_input_data
+from imageio import imread
+from matplotlib import pyplot as plt
+import sys
+
 
 def main():
+
+    # Read the input file (given as a command line argument)
+    # for the digits to use, their names, 
+    # filenames and their classes
+    input_file = sys.argv[1]
+    digits, tags, filenames, image_classes = load_input_data(input_file)
+
+    n_testimages = 500
+    first_digit, first_digit_test, second_digit, second_digit_test = create_train_test_split(digits, n_testimages)
+    """
     x_train, y_train, x_test, y_test = mnist.load()
 
     # create masks for picking the images with right digits
     masks_train = create_masks(y_train)
 
     # create arrays of images
-    zeros = x_train[masks_train[0]].T
-    fours = x_train[masks_train[4]].T
+    # the dimensions are n x m so the matrix needs to be transposed
+    first_digit = x_train[masks_train[digits[0]]].T
+    second_digit = x_train[masks_train[digits[1]]].T
 
     # masks for a few test images
     masks_test = create_masks(y_test)
-    z1 = plt.imread("zero1.png")
-    z2 = plt.imread("zero2.png")
-    z3 = plt.imread("four.png")
-    plt.imshow(z3, cmap='gray')
-    plt.show()
+
+    # Read the digits for the actual classification task
+    z1 = imread(filenames[0])
+    z2 = imread(filenames[1])
+    z3 = imread(filenames[2])
 
     n_testimages = 500
-    # create arrays of test  images
-    zeros_test = x_test[masks_test[0]][0:n_testimages]
-    fours_test = x_test[masks_test[4]][0:n_testimages]
 
+    # create arrays of test  images
+    first_digit_test = x_test[masks_test[digits[0]]][0:n_testimages]
+    second_digit_test = x_test[masks_test[digits[1]]][0:n_testimages]
+    """
     # calculate singular value decompositions for digits zero and four
-    u0, _s0, _v0 = svd(zeros)
-    u4, _s4, _v4 = svd(fours)
+    u1, _s1, _v1 = svd(first_digit)
+    u2, _s2, _v2 = svd(second_digit)
 
     # visualize eigenpatterns for digit zero
-    visualize_eigenpatterns(u0, 3, "zero")
+    visualize_eigenpatterns(u1, 3, tags[0])
 
     # visualize eigenpatterns for digit four
-    visualize_eigenpatterns(u4, 3, "four")
+    visualize_eigenpatterns(u2, 3, tags[1])
 
     k = 20
 
-    # Test if zeros are correctly classified
-    results = np.zeros(n_testimages)
-    for i, zero in enumerate(zeros_test):
-        results[i] = classify_image(zero, u0, u4, "zero", "four", k)
+    # use the zeros and fours from the testset to test the performance
+    test_performance(first_digit_test, second_digit_test, digits, tags, n_testimages, u1, u2, k)
 
-    correct = results == 0
-    print(f"{np.sum(correct)/n_testimages * 100}% correct for zeros")
+    # Classify the provded images
+    classifications = classify_images(filenames, u1, u2, k, digits, image_classes)
 
-    # Test if fours are correctly classified
-    results = np.zeros(n_testimages)
-    for i, four in enumerate(fours_test):
-        results[i] = classify_image(four, u0, u4, "zero", "four", k)
-
-    correct = results == 1
-    print(f"{np.sum(correct)/n_testimages * 100}% correct for fours")
-
-    print(classify_image(z1, u0, u4, "zero", "four", k))
-    print(classify_image(z2, u0, u4, "zero", "four", k))
-    print(classify_image(z3, u0, u4, "zero", "four", k))
-
+    # Print out the classification results
+    assess_classification_results(classifications, image_classes, filenames)
 
 if __name__ == "__main__":
     main()
--- functions.py
+++ functions.py
@@ -1,7 +1,120 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy.linalg import norm
+import mnist
 
+
+def load_input_data(input_filename):
+
+    """
+    Reads the content of the input file to variables.
+
+    The firt line of the input file should contain the two integers
+    which the images are wanted to classify as, separated by spaces.
+
+    The second line should contain the  names of the digits,
+    for example "zero", separated by spaces.
+
+    The third line should contain the filenames of the images that will
+    be classified, separated by spaces.
+
+    The fourth and final line should contain, as integers, the whether
+    the images belong to the first class, given in the first line, or
+    the second class. Denote first class with 0, second class with 1,
+    and separate withspaces.
+
+    Returns: lists of the digits, names of digits, filenames and clases
+    """
+
+    with open(input_filename, "r") as input_file:
+        for i, line in enumerate(input_file):
+            if i == 0:
+                digits = map(int, line.split())
+            elif i == 1:
+                tags = line.split()
+            elif i == 2:
+                filenames = line.split()
+            elif i == 3:
+                image_classes = map(int, line.split())
+
+    return list(digits), tags, filenames, list(image_classes)
+
+
+def pick_digit_from_data(x, y, digit):
+
+    mask = create_masks(y)
+
+    return x[mask[digit]]
+
+def create_train_test_split(digits, n_testimages, transpose_trainig=True, transpose_test=False):
+
+    x_train, y_train, x_test, y_test = mnist.load()
+
+    d1_train = pick_digit_from_data(x_train, y_train, digits[0])
+    d1_test = pick_digit_from_data(x_test, y_test, digits[0])[0:n_testimages]
+
+
+    d2_train = pick_digit_from_data(x_train, y_train, digits[1])
+    d2_test = pick_digit_from_data(x_test, y_test, digits[1])[0:n_testimages]
+
+    if transpose_trainig:
+        d1_train = d1_train.T
+        d2_train = d2_train.T
+
+    if transpose_test:
+        d1_test = d1_test.T
+        d2_test = d2_test.T
+
+    return d1_train, d1_test, d2_train, d2_test
+
+
+def get_class_mappings():
+    digit_names = "zero one two three four five six seven eight nine ten".split()
+    mappings = {}
+
+    for i in range(11):
+        mappings[i] = digit_names[i]
+
+    return mappings
+
+
+def read_images(filenames):
+    z = []
+    for file in filenames:
+        image = plt.imread(file)
+        z.append(image)
+    return z
+
+
+def flatten_images(z):
+    for i, image in enumerate(z):
+        z[i] = image.flatten()
+
+    return z
+
+
+def classify_images(filenames, u1, u2, k, training_classes, image_classes):
+
+    z = read_images(filenames)
+    z_flattened = flatten_images(z)
+
+    classification_results = []
+    for i, image in enumerate(z_flattened):
+        classification_results.append(classify_image(image, u1, u2, k, training_classes))
+
+    return classification_results
+
+
+def assess_classification_results(classifications, image_classes, filenames):
+    digit_mappings = get_class_mappings()
+    for i, classification in enumerate(classifications):
+        correct_class = image_classes[i]
+        if classification == -1:
+            print(f"{filename[i]} could not be classified")
+        elif classification == correct_class:
+            print(f"{filenames[i]} correctly classified as a {digit_mappings[correct_class]}!")
+        else:
+            print(f"{filenames[i]} misclassified as a {digit_mappings[classification]}!")
 
 def create_masks(labels):
 
@@ -78,19 +191,36 @@ def visualize_eigenpatterns(u, k, pattern):
     for i in range(k):
         plt.imshow(u[:, i].reshape((28, 28)), cmap='gray')
         plt.title(f"Eigenpattern {i+1} for digit {pattern}")
+        plt.savefig(f"{pattern}{i}.jpg")
         plt.show()
 
 
-def classify_image(z, u1, u2, pattern1, pattern2, k):
+def classify_image(z, u1, u2, k, classes):
 
     r1 = calculate_residual(z, u1[:, 0:k])
     r2 = calculate_residual(z, u2[:, 0:k])
-    if r1 < r2:
-        #print(f"Image is digit {pattern1}!")
-        return 0
-    elif r2 < r1:
-        #print(f"Image is digit {pattern2}!")
-        return 1
 
-    print("Can't classify image")
+    if r1 < r2:
+        return classes[0]
+    if r2 < r1:
+        return classes[1]
+
     return -1
+
+
+def test_performance(digit_1_test, digit_2_test, classes, tags, n_testimages, u0, u4, k):
+
+    results = np.zeros(n_testimages)
+    for i, image in enumerate(digit_1_test):
+        results[i] = classify_image(image, u0, u4, k, classes)
+
+    correct = results == classes[0]
+    print(f"{np.sum(correct)/n_testimages * 100}% correct for {tags[0]}s")
+
+    # Test if fours are correctly classified
+    results = np.zeros(n_testimages)
+    for i, image in enumerate(digit_2_test):
+        results[i] = classify_image(image, u0, u4, k, classes)
+
+    correct = results == classes[1]
+    print(f"{np.sum(correct)/n_testimages * 100}% correct for {tags[1]}s")
